a0f36362a9c47b5fcc2dd34f152aaee7870b6667
Lin <linjunwen@LindeMacBook-Pro.local>
2017-06-12 16:51:53 +0800

    update

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
+import config from './utils/config';
 import init from './initialize/index';
 
 import SplashScreen from 'react-native-splash-screen';
@@ -16,8 +17,10 @@ export default class Root extends Component {
 	constructor(props){
 		super(props);
 	}
+	componentWillMount(){
+		wechat.registerApp(config.wechatAppID);
+	}
 	componentDidMount(){
-		wechat.registerApp('wx6ce431662d339a23');
 		SplashScreen.hide();
 	}
 	render(){
--- app/utils/config.js
+++ app/utils/config.js
@@ -1,3 +1,4 @@
 export default config = {
-  urlPath: 'http://192.168.1.127:8080/fishsea/'
+  urlPath: 'http://192.168.1.127:8080/fishsea/',
+  wechatAppID: 'wx6ce431662d339a23'
 }
