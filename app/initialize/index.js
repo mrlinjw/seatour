47620f069771eeb48893fd9ab17b7cb7306b9eb3
Lin <linjunwen@LindeMacBook-Pro.local>
2017-06-20 10:20:26 +0800

    add evn

--- app/initialize/index.js
+++ app/initialize/index.js
@@ -3,6 +3,16 @@ import initDate from './date'
 import initStroage from './stroage'
 
 export default function initData(){
+  //__DEV__为React Native全局变量，用于指示当前运行环境是否是开发环境,正式环境取消控制台打印
+  if (!__DEV__) {
+    global.console = {
+      info: () => {},
+      log: () => {},
+      warn: () => {},
+      debug: () => {},
+      error: () => {},
+    };
+  }
   //持久化存储
   initStroage();
   //初始化日历数据
--- app/utils/config.js
+++ app/utils/config.js
@@ -1,4 +1,16 @@
-export default config = {
-  urlPath: 'http://192.168.0.117:8080/fishsea/',
-  wechatAppID: 'wx6ce431662d339a23'
+let config = {};
+//__DEV__为React Native全局变量，用于指示当前运行环境是否是开发环境,
+if(__DEV__){
+  config = {
+    urlPath: 'http://192.168.0.117:8080/fishsea/',
+    wechatAppID: 'wx6ce431662d339a23'
+  }
+}else{
+  config = {
+    urlPath: 'http://www.gotosea.com.cn/fishsea/',
+    wechatAppID: 'wx6ce431662d339a23'
+  }
 }
+
+
+export default config;
